88c6b875c986341744c939182cd5c1e29082c34d
RST Global <rtremblay@hotmail.com>
2025-09-05 21:41:08 -0400

    Create config.ts

---
+++ lib/config.ts
@@ -0,0 +1,12 @@
+// lib/config.ts
+export const PRODUCT_NAME =
+  process.env.NEXT_PUBLIC_PRODUCT_NAME || "MuseMint Supporter Test";
+
+export const PRODUCT_PRICE =
+  process.env.NEXT_PUBLIC_PRODUCT_PRICE || "1.00";
+
+export const PRODUCT_CURRENCY =
+  process.env.NEXT_PUBLIC_PRODUCT_CURRENCY || "CAD";
+
+export const SITE_URL =
+  process.env.SITE_URL || "http://localhost:3000";
